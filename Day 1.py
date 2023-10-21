4fff5f723e46083d514f803f75ee0526dd693803
rahul <rahul>
2023-10-21 16:43:07 -0700

    Solve second half of Day 1 Challenge

--- Day 1.py
+++ Day 1.py
@@ -1,8 +1,16 @@
 # Solving Day 1: Calorie Counting
+#
+# We want to find the total number of calories held by
+# 1) The elf carrying the most calories
+# 2) The three elves carrying the most calories
+#
+# Input file consists of blankline-delimited blocks of data. Each
+# block of data represents the pack of a single elf, and is a
+# newline-delimited list of item calorie counts.
 
 FILENAME = "Day 1 Input.txt"
 
-max_calories = 0
+elf_pack_totals = []
 
 with open(FILENAME, 'r') as file:
     current_elf_calories = 0
@@ -10,11 +18,11 @@ with open(FILENAME, 'r') as file:
         try:
             current_elf_calories += int(line)
         # When we hit a newline character, an error is thrown, at
-        # which point we have seen the entirety of that elf's pack. So
-        # we compare the current elf's calories with the maximum and
-        # replace if necessary
+        # which point we have seen the entirety of that elf's pack
         except:
-            max_calories = max(max_calories, current_elf_calories)
+            elf_pack_totals.append(current_elf_calories)
             current_elf_calories = 0
 
-print(max_calories)
+elf_pack_totals.sort(reverse=True)
+max_calories = elf_pack_totals[0]
+top_three = elf_pack_totals[0:3]
