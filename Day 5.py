c56eae0a11158f784c45fbb4be8c79eedffcbadb
rahul <rahul>
2023-10-22 15:30:58 -0700

    Day 5 Part 2 Solved

--- Day 5.py
+++ Day 5.py
@@ -3,9 +3,15 @@
 from collections import deque
 
 FILENAME = "Day 5 Input.txt"
+# Choose which part of the challenge is being solved
+part_one = False
+part_two = True
 
 n_stacks = 9
 stacks = {i:deque() for i in range(1,n_stacks + 1)}
+# Add a special "temp" stack for use in part two
+if part_two:
+    stacks["temp"] = deque()
 max_stack_height = 8
 
 def ingest_across_stacks(line):
@@ -14,9 +20,20 @@ def ingest_across_stacks(line):
         if char != ' ':
             stacks[i + 1].appendleft(char)
             
-def move(quantity, source, destination):
+def move_one_at_a_time(quantity, source, destination):
     for i in range(quantity):
         stacks[destination].append(stacks[source].pop())
+
+def move_in_bulk(quantity, source, destination):
+    """
+    Bulk movement of crates for part two
+
+    Implemented as moving one at a time to a special temp storage, and
+    then from the temp storage to the destination
+
+    """
+    move_one_at_a_time(quantity, source, "temp")
+    move_one_at_a_time(quantity, "temp", destination)
 
 with open(FILENAME) as f:
     data = f.read().split("\n")
@@ -29,8 +46,12 @@ for i in range(max_stack_height):
 # [NUMBER_TO_MOVE, SOURCE, DESTINATION]
 instructions = [[int(i) for i in line.split(" ") if i.isdecimal()] for line in data[10:]]
 
-for command in instructions:
-    move(*command)
+if part_one:
+    for command in instructions:
+        move_one_at_a_time(*command)
+if part_two:
+    for command in instructions:
+        move_in_bulk(*command)
 
 top = [stacks[i][-1] for i in range(1,n_stacks + 1)]
 print("".join(top))
